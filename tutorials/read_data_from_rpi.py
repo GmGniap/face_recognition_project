16b18baa28bfd5e3ba2ba7a9d90675894226eccd
gmgniap <thetpaing@utycc.edu.mm>
2024-05-25 05:12:24 +0630

    Tutorial updates

--- tutorials/read_data_from_rpi.py
+++ tutorials/read_data_from_rpi.py
@@ -6,7 +6,7 @@ pico_uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 led = Pin(16, Pin.OUT)
 
 while True:
-    if pico_uart.any():
+    if pico_uart.any(): ## can use alter : while pico_uart.any() > 0:
         
         msg = pico_uart.read().decode()
         print(f"Receiving msg {msg} from Rpi ...")
